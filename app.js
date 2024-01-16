422b766460aac35368e24d9daee3f5b7ccc71ad2
Niladri Das <niladri.das@lpu.in>
2024-01-17 01:55:23 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Example: Broadcast a random number every second
+  setInterval(() => {
+    const randomNumber = Math.floor(Math.random() * 100);
+    io.emit('update', { number: randomNumber });
+  }, 1000);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
